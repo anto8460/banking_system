f9c8d6c8cab9c0900d313c5af0cdaf977eb182b2
RafaelCristianBarbieru <baracri00@gmail.com>
2022-03-11 14:36:42 +0100

    Shit

--- banking_system/banking_system/urls.py
+++ banking_system/banking_system/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('', include('banking_system_app.urls')),
     path('login/', include('login_app.urls')),
