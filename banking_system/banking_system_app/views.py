f700220a7dba48798e3c5fc475ba956cc0117a18
akQuadSat <ak@quadsat.com>
2022-03-11 15:08:29 +0100

    Latest Changes

--- banking_system/banking_system_app/views.py
+++ banking_system/banking_system_app/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from .models import Customer, Employee, Account
 from django.shortcuts import render
 
 
@@ -6,9 +8,20 @@ def home(request):
 
     contex = {}
 
+    if request.user:
+        user = request.user
+        customer = Customer.objects.filter(user=user)
+
+        if customer:
+            # accounts = Account.filter.get(customer=customer)
+            # print(accounts)
+            ...
+        else:
+            employee = Employee.objects.filter(user=user)
+        return HttpResponse('You are logged with ')
+
     if request.method == 'POST':
         ...
 
-    return HttpResponse('You are logged in')
     # return render(request, 'index.html')
 
