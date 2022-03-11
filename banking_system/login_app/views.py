c12a012005267a5d25166c93f298e369a6ebdb80
akQuadSat <ak@quadsat.com>
2022-03-11 11:37:20 +0100

    Login-rout

--- banking_system/login_app/views.py
+++ banking_system/login_app/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.contrib.auth import authenticate, login as dj_login, logout as dj_logout
+from django.shortcuts import HttpResponseRedirect
 
+def login(request):
+    context = {}
+    if request.method == 'POST':
+        user = authenticate(request, username=request.POST['user'], password=request.POST['password'])
+        if user:
+            dj_login(request, user)
+            return HttpResponseRedirect(reverse('banking_system:index'))
+        else:
+            context = {
+                'error': 'Wrong username or password!'
+            }
 
-def index(request):
     return render(request, 'login.html')
 
