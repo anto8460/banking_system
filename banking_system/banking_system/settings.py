6389d5699153da35ea4281ce962bd4101128133b
akQuadSat <ak@quadsat.com>
2022-03-25 10:15:02 +0100

    Move ecret key to env file

--- banking_system/banking_system/settings.py
+++ banking_system/banking_system/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5iw9o=wvo2b2f&+%xy)v2v&6b8(_1s6tdx9avl9ue^1=ys#mo+'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
