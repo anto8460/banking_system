582c844af9056dc266a29a01119891e65afc6e95
akQuadSat <ak@quadsat.com>
2022-04-01 09:33:35 +0200

    Changes db to Postgres

--- banking_system/banking_system_app/models.py
+++ banking_system/banking_system_app/models.py
@@ -11,7 +11,7 @@ from django.contrib.auth.models import User
 
 
 class AccountType(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     type = models.CharField(unique=True, max_length=255)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField(blank=True, null=True)
@@ -20,28 +20,22 @@ class AccountType(models.Model):
         return f'AccountType - {self.type}'
 
     class Meta:
-        managed = False
         db_table = 'account_types'
 
 
 class Account(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     account_type = models.ForeignKey(AccountType, models.DO_NOTHING)
     customer = models.ForeignKey('Customer', models.DO_NOTHING)
     account_number = models.CharField(unique=True, max_length=255)
-    # balance = models.FloatField()
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField(blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'accounts'
 
     def __str__(self):
         return f'Account - {self.account_number}'
-
-    def get_balance(self):
-        ...
 
 
 class AccountsTransaction(models.Model):
@@ -49,19 +43,17 @@ class AccountsTransaction(models.Model):
     transaction = models.ForeignKey('Transaction', models.DO_NOTHING)
 
     class Meta:
-        managed = False
         db_table = 'accounts_transactions'
         unique_together = (('account', 'transaction'),)
 
 
 class BankDetail(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     account = models.ForeignKey(Account, models.DO_NOTHING)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField(blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'bank_details'
 
     def __str__(self):
@@ -69,8 +61,8 @@ class BankDetail(models.Model):
 
 
 class Customer(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(unique=True, max_length=255)
@@ -78,28 +70,26 @@ class Customer(models.Model):
     age = models.IntegerField()
     phone_number = models.CharField(unique=True, max_length=255)
     created_at = models.DateTimeField()
-    updated_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField()
 
     class Meta:
-        managed = False
         db_table = 'customers'
 
     def __str__(self):
-        return f'Customer - {self.first_name} {self.last_name}: UserId {self.user.username}'
+        return f'Customer - {self.first_name} {self.last_name}: UserId {self.user}'
 
 
 class Employee(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(unique=True, max_length=255)
     cpr = models.CharField(unique=True, max_length=8)
     created_at = models.DateTimeField()
-    updated_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField()
 
     class Meta:
-        managed = False
         db_table = 'employees'
 
     def __str__(self):
@@ -107,14 +97,13 @@ class Employee(models.Model):
 
 
 class Loan(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     customer = models.ForeignKey(Customer, models.DO_NOTHING)
     amount = models.FloatField()
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
     class Meta:
-        managed = False
         db_table = 'loans'
 
     def __str__(self):
@@ -122,11 +111,10 @@ class Loan(models.Model):
 
 
 class Transaction(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     amount = models.FloatField()
     text = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField()
 
     class Meta:
-        managed = False
         db_table = 'transactions'
