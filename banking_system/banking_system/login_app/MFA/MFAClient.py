9d0282d93004f67fd2ffe6e09f164ff704930140
akQuadSat <ak@quadsat.com>
2022-05-17 01:46:33 +0200

    MFA

---
+++ banking_system/banking_system/login_app/MFA/MFAClient.py
@@ -0,0 +1,27 @@
+
+from twilio.rest import Client
+from .Singleton import SingletonMeta
+from decouple import config
+from random import randint
+
+
+class MFAClient(metaclass=SingletonMeta):
+
+    def __init__(self):
+        self.account_sid = config('ACCOUNT_SID')
+        self.auth_token = config('AUTH_TOKEN')
+        self.phone_number = config('PHONE_NUMBER')
+        self.twilio_client = Client(self.account_sid, self.auth_token)
+
+    def send_message(self, message, reciever):
+        message = self.twilio_client.messages.create(
+            body=message,
+            from_=self.phone_number,
+            to=reciever)
+
+        print(message)
+
+    @staticmethod
+    def generate_code() -> str:
+        # Generate random code
+        return str(randint(100_000, 999_999))
